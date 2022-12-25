6293daa5f78e366c4c414fe66d37a913a7c42b2b
Mostafa H <Mahussein04@gmail.com>
2022-12-25 18:08:27 -0500

    router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Backdrop from "./components/backdrop/Backdrop.js"
 import Home from "./components/Home/Home.js" 
 import { useState } from "react";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   const [darkMode, setdarkMode] = useState(true);
 
@@ -25,9 +27,22 @@ function App() {
 
   return (
     <div className="App" style = {appStyle}>
-      <Home darkMode = {darkMode}/>
+      <BrowserRouter >
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home darkMode = {darkMode}/>} /> // home route
+          <Route path = "about" element={<Home darkMode = {darkMode}/>} /> // about route
+          <Route path = "projects" element={<Home darkMode = {darkMode}/>} /> // projects route
+          <Route path = "resume" element={<Home darkMode = {darkMode}/>} /> // resume route
+          <Route path = "interest" element={<Home darkMode = {darkMode}/>} /> // interests route
+          <Route path = "contact" element={<Home darkMode = {darkMode}/>} /> // contact me route
+          
+          <Route path="*" element={<Home darkMode = {darkMode}/>} /> // page-not-found route
+        </Route>
+      </Routes>
       <Backdrop darkMode = {darkMode} changeDarkMode = {setdarkMode}/>
-    </div>
+    </BrowserRouter>
+  </div>
   );
 }
 
